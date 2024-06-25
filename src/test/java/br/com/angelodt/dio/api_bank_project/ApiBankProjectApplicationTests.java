e642a61243c78a8ffa989d3c7df9ea02b0363607
angelodt <32304707+angelodt@users.noreply.github.com>
2024-06-25 14:31:19 -0300

    Initial commit

---
+++ src/test/java/br/com/angelodt/dio/api_bank_project/ApiBankProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.angelodt.dio.api_bank_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBankProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
